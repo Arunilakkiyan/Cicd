e304b2da82eb24dd08980ce8326b4197f341fc2b
Arunilakkiyan <Arunilakkiyan>
2026-02-17 11:37:20 +0530

    New

--- tests/Api.js
+++ tests/Api.js
@@ -1,4 +1,4 @@
 const { log } = require("node:console");
 
 console.log("Arun Added New File");
-log("This is a sample file for API testing.");
+console.log("This is a sample file for API testing.");
