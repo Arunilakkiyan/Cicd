5d2fc2306866c9ff9856bb6de4cf2776f9f6ec18
Arunilakkiyan <Arunilakkiyan>
2026-02-18 11:34:47 +0530

    Changes

--- playwright.config.js
+++ playwright.config.js
@@ -29,10 +29,12 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
     browserName: 'chromium',
-    headless: false,
+    headless: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    video:'retain-on-failure',
+    screenshot:'only-on-failure'
   },
 
   /* Configure projects for major browsers */
